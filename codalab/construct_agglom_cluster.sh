b51eb5ca074c7bd406d5a6f3dc9a2626cbb78c3b
Tony Lee <tonyh.lee@yahoo.com>
2020-04-22 15:09:16 -0700

    added agglom + reconstruct bash scripts

--- codalab/construct_agglom_cluster.sh
+++ codalab/construct_agglom_cluster.sh
@@ -2,7 +2,7 @@
 # Script to construct agglomerative clusters
 
 echo 'job_id: '$1
-echo 'gamma: '$1
+echo 'gamma: '$2
 
 # Activate at-env conda environment
 source activate atenv
@@ -11,19 +11,22 @@ pip install query
 # conda list
 
 # Set required environment variables
-export CLUSTERER_PATH=$HOME/clusterers/vocab100000_ed1_gamma$2
-
-# Make directory CLUSTERER_PATH
+# Make directory where clusterers will be stored
 mkdir $HOME/clusterers
-mkdir $CLUSTERER_PATH
 
 # Needed for CodaLab as roben is mounted one level down
 cd roben || exit 1
 
 # Make clusterer
-echo 'Constructing a clusterer...'
-python construct_clusters.py --vocab_size 100000 --perturb_type ed1 --data_dir $HOME/data --output_dir $CLUSTERER_PATH
+echo 'Constructing a connected component clusters'
+python construct_clusters.py --vocab_size 100000 --perturb_type ed1 --data_dir $HOME/data --output_dir $HOME/clusterers
+
+#Now set the clusterer path
+export CLUSTERER_PATH=$HOME/clusterers/vocab100000_ed1.pkl
+
+#Make directory where partial agglomerative clusters will be saved
+mkdir $HOME/clusterers/vocab100000_ed1_gamma$2
 
 # We will now construct our more complicated clusters, the agglomerative clusters
-echo 'Building agglomerative cluster...'
-python agglom_clusters.py --gamma $2 --clusterer_path $CLUSTERER_PATH/vocab100000_ed1.pkl --job_id $1 --num_jobs 2
+echo 'Building agglomerative clusters (one of two jobs)...'
+python agglom_clusters.py --gamma $2 --clusterer_path $CLUSTERER_PATH --job_id $1 --num_jobs 2
